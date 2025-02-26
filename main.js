a97c2f6ce198fae00da4ff53f6a6b53176387629
Luh234 <lgo2@aluno.ifnmg.edu.br>
2025-02-26 00:39:54 +0000

    Mensagem do commit

---
+++ main.js
@@ -0,0 +1,24 @@
+const botaoAutor = document.getElementById('botaoAutor');
+const respAutor = document.getElementById('respAutor');
+
+botaoAutor.addEventListener('click', pegarInformacao);
+
+const urlBase = "https://back_end_na_vercel"; // Substitua pela URL do seu Back-End na Vercel
+
+async function pegarInformacao() {
+    respAutor.innerText = "Aguarde...";
+
+    try {
+        const response = await fetch(urlBase);
+
+        if (!response.ok) {
+            throw new Error("Erro na requisição: " + response.status);
+        }
+
+        const data = await response.json();
+        respAutor.innerText = data.nome;
+    } catch (error) {
+        console.error("Erro:", error);
+        respAutor.innerText = `Erro: ${error.message}`;
+    }
+}
